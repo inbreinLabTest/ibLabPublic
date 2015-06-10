6966fd5a4d2e8e723e9ba773f40913e1703f3c60
inbreinLabTest <shadowds@nate.com>
2015-06-10 19:04:26 +0900

    폴더만들기
    XML 파일명으로 폴더만들기

---
+++ CreateReaultXMLDirectory/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CreateReaultXMLDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreate.Location = new System.Drawing.Point(649, 47);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(159, 20);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(565, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "뽀리해온 결과파일위치 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 78);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CreateReaultXMLDirectory/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CreateReaultXMLDirectory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (this.txtPath.Text == "")
+            {
+                MessageBox.Show("결과 파일들이 있는 경로를 입력해주세요");
+                return;
+            }
+
+
+            string resultXMLPath = this.txtPath.Text;
+
+            DirectoryInfo resultPathDirectory = new DirectoryInfo(resultXMLPath);
+
+
+            foreach (System.IO.FileInfo f in resultPathDirectory.GetFiles())
+            {
+                string resultFileName = f.Name;
+
+                string projectNum = resultFileName.Replace("Result_", "").Replace(f.Extension, "");
+
+                if (f.Extension == ".xml")
+                { 
+                    //
+                    string projectPath = f.DirectoryName + "\\"+projectNum;
+
+                    if (File.Exists(projectPath) == false)
+                        System.IO.Directory.CreateDirectory(projectPath);
+
+                    string projectSimPath = projectPath + "\\SimXml";
+
+                    if (File.Exists(projectSimPath) == false)
+                        System.IO.Directory.CreateDirectory(projectSimPath);
+
+                    //경로에서 읽어와서
+                    FileInfo resultFile = new FileInfo(f.FullName);
+
+                    if (File.Exists(projectSimPath + "\\" + resultFileName) == false)
+                        resultFile.MoveTo(projectSimPath + "\\" + resultFileName);
+                    else
+                        resultFile.MoveTo(projectSimPath + "\\" + resultFileName + ".bak");
+                }
+            }
+
+            MessageBox.Show("끝");
+        }
+
+       
+    }
+}

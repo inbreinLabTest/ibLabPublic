ab83f448e7f7a05c1534d769b162909f389f31d8
inbreinLabTest <shadowds@nate.com>
2015-06-10 22:11:18 +0900

    dddddd
    dddd

--- CreateReaultXMLDirectory/Program.cs
+++ CreateReaultXMLDirectory/Program.cs
@@ -13,7 +13,8 @@ namespace CreateReaultXMLDirectory
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+ 
+            Application.EnableVisualStyles();           Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
